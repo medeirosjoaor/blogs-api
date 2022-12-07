6bba07fc5c91c910f49c7ac57f8db9c1e052b39a
João Renato <nateaorooj@gmail.com>
2022-12-06 21:38:36 -0300

    feat: requirement 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,37 @@
+function PostCategoryModel(sequelize, DataTypes) {
+  const PostCategory = sequelize.define('PostCategory', {
+    categoryId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    postId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    }
+  },
+  {
+    tableName: 'users',
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blog_posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    });
+  }
+
+  return PostCategory;
+}
+
+module.exports = PostCategoryModel;
